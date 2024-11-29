31ac7e3f8126030d106356211332af334e5d76b1
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2024-11-29 23:53:29 +0530

    Time: 16 ms (5.15%), Space: 8.3 MB (90.43%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int d = x, rev = 0;
+
+        while (d > 0) {
+            if(rev>(INT_MAX/10)||rev<(INT_MIN/10)) 
+             return false;
+          
+            rev = (rev* 10) + (d % 10);
+            d /= 10;
+        }
+        cout<<rev;
+
+        return x==rev;
+    }
+};
