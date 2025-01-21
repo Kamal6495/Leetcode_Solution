914e500a89a6bcfd2a0245a386316626b3960929
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-01-22 00:18:46 +0530

    Time: 8 ms (24.43%), Space: 79.6 MB (7.69%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,21 +1,12 @@
 class Solution {
-public:
-    void printGrid(const vector<vector<long long>>& matrix) {
-        for (const auto& row : matrix) {
-            for (long long val : row) {
-                cout << val << " ";
-            }
-            cout << endl;
-        }
-    }
-
 
 public:
     long long gridGame(vector<vector<int>>& grid) {
-long long res = LLONG_MAX; // Initialize to maximum possible value
+        long long res = LLONG_MAX; // Initialize to maximum possible value
         size_t n = grid[0].size(); // Use size_t for size
 
-        vector<vector<long long>> presum(2, vector<long long>(n, 0)); // Use long long for prefix sums
+        vector<vector<long long>> presum(
+            2, vector<long long>(n, 0)); // Use long long for prefix sums
         presum[0][0] = 0;
         presum[1][0] = grid[1][0];
         for (size_t i = 1; i < n; i++) {
@@ -25,20 +16,19 @@ long long res = LLONG_MAX; // Initialize to maximum possible value
 
         presum[1][n - 1] = 0; // Logic here may need revisiting
 
-         res = min(res, presum[0][n - 1]);
+        res = min(res, presum[0][n - 1]);
 
- //cout<<"RES==>"<<res;
+        // cout<<"RES==>"<<res;
         for (size_t i = 1; i < n; i++) {
             long long r0 = presum[0][n - 1] - presum[0][i];
 
-            
-            long long r1 = presum[1][i-1];
-           // cout<<"RO= "<<r0<<"R1= "<<r1<<endl;
-            
+            long long r1 = presum[1][i - 1];
+            // cout<<"RO= "<<r0<<"R1= "<<r1<<endl;
+
             res = min(res, max(r0, r1));
         }
 
-       // printGrid(presum);
+        // printGrid(presum);
         return res;
     }
 };
