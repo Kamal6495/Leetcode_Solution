4f0e99c1a64f2adc1c5d4a2e42072304943a9b8a
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-01-24 02:19:49 +0530

    Time: 2 ms (100%), Space: 53 MB (8.87%) - LeetHub

---
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countServers(int[][] grid) {
+        int res=0;
+      int m=grid.length;
+      int n=grid[0].length;
+
+       int row[]=new int[m];
+       int col[]=new int[n];
+
+      for(int i=0;i<m;i++){
+for(int j=0;j<n;j++)
+{
+if(grid[i][j]==1)
+{
+row[i]++;col[j]++;
+
+}
+
+
+}
+      }
+
+
+            for(int i=0;i<m;i++){
+for(int j=0;j<n;j++){
+
+if(grid[i][j]==1 &&(row[i]>1||col[j]>1))
+res++;
+
+}}
+
+
+     
+        return res;
+    }
+}
