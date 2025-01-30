97d052cd945f66604deb6a08b72754690e6c6038
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-01-31 02:30:12 +0530

    Time: 1 ms (99.8%), Space: 44.6 MB (94.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -6,13 +6,22 @@ class Solution {
             Arrays.fill(board[i], '.'); // Fill each row with '.'
         }
 
-        solve(ans, board, 0, n);
+        int lr[] = new int[n];
+        int ld[] = new int[2 * n - 1];
+        int ud[] = new int[2 * n - 1];
+
+        Arrays.fill(lr, 0);
+        Arrays.fill(ld, 0);
+        Arrays.fill(ud, 0);
+
+        solve(ans, board, 0, n,lr,ld,ud);
 
         return ans;
 
     }
 
-    public void solve(List<List<String>> ans, char[][] board, int col, int n) {
+    // lr=leftroe,ld=lowerdiagonal,ud=upperdiagonal
+    public void solve(List<List<String>> ans, char[][] board, int col, int n, int lr[], int ld[], int ud[]) {
 
         if (col == n) {
             List<String> temp = new ArrayList<>();
@@ -25,41 +34,59 @@ class Solution {
 
         for (int row = 0; row < n; row++) {
 
-            if (isSafe(row, col, board)) {
+            if (lr[row] == 0 && ld[row + col] == 0 && ud[n - 1 - (row - col)] == 0) {
                 board[row][col] = 'Q';
-                solve(ans, board, col + 1, n);
+                lr[row] = 1;
+                ld[row + col] = 1;
+                ud[n - 1 - (row - col)] = 1;
+
+                solve(ans, board, col + 1, n, lr, ld, ud);
+
                 board[row][col] = '.';
+                lr[row] = 0;
+                ld[row + col] = 0;
+                ud[n - 1 - (row - col)] = 0;
 
             }
+            /*
+             * if (isSafe(row, col, board)) {
+             * board[row][col] = 'Q';
+             * solve(ans, board, col + 1, n);
+             * board[row][col] = '.';
+             * 
+             * }
+             */
         }
 
     }
 
-    public boolean isSafe(int row, int col, char[][] board) {
-        int rr = row;
-        int cc = col;
-        // left row
-        while (cc>= 0) {
-            if (board[rr][cc] == 'Q')
-                return false;
-            cc--;
-        }
-        rr = row;
-        cc = col;
-        while (rr >= 0 && cc >= 0) {
-            if (board[rr][cc] == 'Q')
-                return false;
-            rr--;
-            cc--;
-        }
-        rr = row;
-        cc = col;
-        while (rr < board.length && cc >= 0) {
-            if (board[rr][cc] == 'Q')
-                return false;
-            rr++;
-            cc--;
-        }
-        return true;
-    }
+    /*
+     * public boolean isSafe(int row, int col, char[][] board) {
+     * int rr = row;
+     * int cc = col;
+     * // left row
+     * while (cc>= 0) {
+     * if (board[rr][cc] == 'Q')
+     * return false;
+     * cc--;
+     * }
+     * rr = row;
+     * cc = col;
+     * while (rr >= 0 && cc >= 0) {
+     * if (board[rr][cc] == 'Q')
+     * return false;
+     * rr--;
+     * cc--;
+     * }
+     * rr = row;
+     * cc = col;
+     * while (rr < board.length && cc >= 0) {
+     * if (board[rr][cc] == 'Q')
+     * return false;
+     * rr++;
+     * cc--;
+     * }
+     * return true;
+     * }
+     */
 }
