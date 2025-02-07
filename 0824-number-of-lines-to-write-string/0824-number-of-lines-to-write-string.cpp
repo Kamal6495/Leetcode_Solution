1acbb4356293f1f9c1d8ca50854b480ff638a1b7
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-02-08 01:10:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (100%) - LeetHub

--- 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.cpp
+++ 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.cpp
@@ -1,19 +1,19 @@
-;
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
-
         int sum = 0, line = 1;
+
         for (char c : s) {
             int width = widths[c - 'a'];
 
-            if (sum + width > 100) {
-                line++;
-                sum = width;
+            if (sum + width > 100) { // Check before adding width
+                line++; 
+                sum = width; // Start new line with the current character's width
             } else {
-                sum += width;
+                sum += width; // Add width normally
             }
         }
+
         return {line, sum};
     }
 };
