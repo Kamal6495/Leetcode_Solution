bf9c2e9ebc8d88b5430fca349c1a080584bf4df1
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-04-09 15:58:58 +0530

    Time: 12 ms (55.35%), Space: 32.4 MB (57.55%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         
-        set<int> s;
+        unordered_set<int> s;
         for(int i=0;i<nums.size();i++){
             if(nums[i]<k)
             return -1;
