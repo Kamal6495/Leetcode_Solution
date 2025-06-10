ac9f3ba4f1ba4c7dbcbd3ca1079236c63ac5147e
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-06-10 15:11:39 +0530

    Time: 2 ms (99.54%), Space: 50.4 MB (35.2%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,21 +1,42 @@
 class Solution {
-    public List<Integer> majorityElement(int[] arr) {
-        int n = arr.length;
+    public List<Integer> majorityElement(int[] nums) {
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int num : nums) {
+            if (cnt1 == 0 && ele2 != num) {
+                cnt1 = 1;
+                ele1 = num;
+            } else if (cnt2 == 0 && ele1 != num) {
+                cnt2 = 1;
+                ele2 = num;
+            } else if (ele1 == num)
+                cnt1++;
+            else if (ele2 == num)
+                cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
+
+        }
         List<Integer> ls = new ArrayList<>();
-int minelements=(n/3)+1;
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        for (int num : arr) {
-            mp.put(num, mp.getOrDefault(num, 0) + 1);
-            int x=mp.get(num);
-            if(mp.get(num)==minelements)
-            ls.add(num);
-
-            if(ls.size()==2) break;
-        
+        cnt1 = 0;
+        cnt2 = 0;//Reinitialize;
+        for (int num : nums) {
+            if (ele1 == num)
+                cnt1++;
+            if (ele2 == num)
+                cnt2++;
         }
 
+        int minelement = (n / 3) + 1;
 
+        if (cnt1 >= minelement)
+            ls.add(ele1);
+        if (cnt2 >= minelement)
+            ls.add(ele2);
         return ls;
+
     }
 }
