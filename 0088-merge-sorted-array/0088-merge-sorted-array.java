acc6e2a9dd8262f0da40f242c27b1f0b343543f1
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-06-16 16:15:07 +0530

    Time: 1 ms (27.71%), Space: 42.3 MB (42.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,18 +1,17 @@
 class Solution {
     //Using Shell Sort Gap Method , Optimal Solution 2
     public void merge(int[] arr1, int m, int[] arr2, int n) {
-        int m1=m;
+        int m1 = m;
         for (int num : arr2) {
             arr1[m1++] = num;
         }
-      //  System.out.println(Arrays.toString(arr1));
         // int gap=(int)Math.ciel((m+n)*0.5);
         int gap = ((m + n) / 2) + ((m + n) % 2);
         while (gap >= 1) {
-           
+
             int left = 0;
             int right = gap;
-            while (right < m+n) {
+            while (right < m + n) {
                 if (arr1[left] > arr1[right]) {
                     int temp = arr1[left];
                     arr1[left] = arr1[right];
@@ -22,7 +21,8 @@ class Solution {
                 left++;
                 right++;
             }
-            if(gap==1) break;
+            if (gap == 1)
+                break;
             gap = (gap / 2) + (gap % 2);
         }
     }
