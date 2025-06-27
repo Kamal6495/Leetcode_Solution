e5fc4d040c1c56b88a1a45be9444efab20332adb
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-06-27 15:46:21 +0530

    Time: 44 ms (64.68%), Space: 55 MB (53.1%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -5,20 +5,22 @@ class Solution {
         if (low >= high)
             return cnt;
         int mid = low + ((high - low) / 2);
-        cnt+=mergeSort(arr, low, mid);
-        cnt+=mergeSort(arr, mid + 1, high);
-        cnt+=countPairs(arr,low,mid,high);
+        cnt += mergeSort(arr, low, mid);
+        cnt += mergeSort(arr, mid + 1, high);
+        cnt += countPairs(arr, low, mid, high);
         merge(arr, low, mid, high);
         return cnt;
     }
-    public int countPairs(int arr[],int low,int mid,int high){
-        int right=mid+1;
-        int cnt=0;
 
-        for(int left=low;left<=mid;left++){
-        
-        while(right<=high && (long)arr[left]>arr[right]*2L) right++;
-        cnt+=(right-(mid+1));
+    public int countPairs(int arr[], int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+
+        for (int left = low; left <= mid; left++) {
+
+            while (right <= high && (long) arr[left] > arr[right] * 2L)
+                right++;
+            cnt += (right - (mid + 1));
         }
         return cnt;
     }
@@ -34,12 +36,6 @@ class Solution {
         for (int i = 0; i < n2; i++)
             right[i] = arr[mid + i + 1];
 
-        // int rr = mid + 1;
-        // for (int ll = low; ll <= mid; ll++) {
-        //     while ((rr <= high) && ((long) arr[ll] > 2L * arr[rr]))
-        //         rr++;
-        //     cnt += (rr - (mid + 1));
-        // }
 
         int i = 0, j = 0, k = low;
         while (i < n1 && j < n2) {
