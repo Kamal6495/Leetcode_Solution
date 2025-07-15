b30fb35e571172f90b25c45f73925261762536cb
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-07-15 11:20:14 +0530

    Time: 0 ms (100%), Space: 46.4 MB (8.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,34 @@
 class Solution {
-    //Binary Method T(n)= O(2Log(n))
-    //Using Upper and Lower Bound Concept
-
-    public int LB(int[] arr, int n, int target) {
-        int low = 0;
-        int high = n - 1;
-        int ans = n;
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (arr[mid] >= target) {
-                ans = mid;
-                high = mid - 1;
-            } else
-                low = mid + 1;
-        }
-        return ans;
-    }
-
-    public int UB(int[] arr, int n, int target) {
-        int low = 0;
-        int high = n - 1;
-        int ans = n;
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (arr[mid] > target) {
-                ans = mid;
-                high = mid - 1;
-            } else
-                low = mid + 1;
-        }
-        return ans;
-    }
-
     public int[] searchRange(int[] nums, int target) {
-        int lb = LB(nums, nums.length, target);
-        if (lb == nums.length || nums[lb] != target)
-            return new int[] { -1, -1 };
+        int low = 0;
+        int high = nums.length - 1;
+        int first = -1;
+        int last = -1;
 
-        int ub = UB(nums, nums.length, target);
-
-        return new int[] { lb, ub - 1 };
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        low = 0;
+        high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return new int[] { first, last };
     }
 }
