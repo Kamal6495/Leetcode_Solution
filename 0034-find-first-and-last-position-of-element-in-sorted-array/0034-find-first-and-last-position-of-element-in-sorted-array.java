27b228dcc2af08ead36085bfe3006ea89e937216
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-07-15 10:47:40 +0530

    Time: 0 ms (100%), Space: 45.8 MB (93.69%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,44 @@
 class Solution {
-    //Linear Method T(n)= O(n)
-    public int[] searchRange(int[] nums, int target) {
-        int F = -1, L = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                if (F == -1)
-                    F = i;
-                L = i;
-            }
-        }
+    //Binary Method T(n)= O(2Log(n))
+    //Using Upper and Lower Bound Concept
 
-        return new int[] { F, L };
+    public int LB(int[] arr, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return ans;
+    }
+
+    public int UB(int[] arr, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int lb = LB(nums, nums.length, target);
+        if (lb == nums.length || nums[lb] != target)
+            return new int[] { -1, -1 };
+
+        int ub = UB(nums, nums.length, target);
+
+        return new int[] { lb, ub - 1 };
     }
 }
