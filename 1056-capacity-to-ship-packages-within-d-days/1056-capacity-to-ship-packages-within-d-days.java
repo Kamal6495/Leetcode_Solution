ec3cbe72cad69f44034139489f49860ef4899369
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-07-22 11:55:46 +0530

    Time: 12 ms (30.67%), Space: 47.2 MB (71.43%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,53 +1,49 @@
 class Solution {
-  public int shipWithinDays(int[] weights, int days) {
-    int low = minA(weights);
-    int high = sumA(weights);
-    int ans = high;
+    public int shipWithinDays(int[] weights, int days) {
+        int low = minA(weights);
+        int high = sumA(weights);
+        int ans = high;
 
-    while (low <= high) {
-      int mid = low + ((high - low) / 2);
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            int requiredDays = reqDays(weights, mid);
 
-      int reqiuredDays = reqDays(weights, mid);
-
-      if (reqiuredDays <= days) { 
-        ans = mid;
-        high = mid - 1;
-      } else {
-        low = mid + 1;
-      }
+            if (requiredDays <= days) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 
-    return ans;
-  }
+    public int reqDays(int[] arr, int cap) {
+        int days = 1;
+        int load = 0;
 
-  public int reqDays(int arr[], int cap) {
-    int days = 1;
-    int load = 0;
-
-    for (int i = 0; i < arr.length; i++) {
-      if ((arr[i] + load) > cap) {
-        days = days + 1;
-        load = arr[i];
-      } else {
-        load += arr[i];
-      }
+        for (int weight : arr) {
+            if (load + weight > cap) {
+                days++;
+                load = weight;
+            } else {
+                load += weight;
+            }
+        }
+        return days;
     }
 
-    return days;
-  }
+    public int minA(int[] arr) {
+        int x = arr[0];
+        for (int num : arr)
+            x = Math.max(x, num);
+        return x;
+    }
 
-  public int minA(int[] arr) {
-    int x = Integer.MIN_VALUE;
-    for (int num : arr)
-      x = Math.max(x, num);
-    return x;
-  }
-
-  public int sumA(int[] arr) {
-    int x = 0;
-    for (int num : arr)
-      x += num;
-    return x;
-  }
-
+    public int sumA(int[] arr) {
+        int sum = 0;
+        for (int num : arr)
+            sum += num;
+        return sum;
+    }
 }
