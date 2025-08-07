8b6b5f1cebbb990383084b88a115c173bba9cf6d
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-08-07 12:27:07 +0530

    Time: 1 ms (100%), Space: 46.4 MB (26.38%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,52 +1,33 @@
 class Solution {
   public double findMedianSortedArrays(int[] arr1, int[] arr2) {
     int n1 = arr1.length, n2 = arr2.length;
-    int i = 0, j = 0, cnt = 0;
-    int idx1 = (n1 + n2) / 2;
-    int idx2 = ((n1 + n2) / 2) - 1;
-    int ele1 = -1;
-    int ele2 = -1;
-    while (i < n1 && j < n2) {
-      if (arr1[i] <= arr2[j]) {
-        if (cnt == idx1)
-          ele1 = arr1[i];
-        if (cnt == idx2)
-          ele2 = arr1[i];
-        i++;
-        cnt++;
+    if (n1 > n2)
+      return findMedianSortedArrays(arr2, arr1);
 
-      } else {
-        if (cnt == idx1)
-          ele1 = arr2[j];
-        if (cnt == idx2)
-          ele2 = arr2[j];
-        j++;
-        cnt++;
+    int low = 0;
+    int high = n1;
+    int left = ((n1 + n2 + 1) / 2);
+    while (low <= high) {
+      int mid1 = low + ((high - low) / 2);
+      int mid2 = left - mid1;
+      int l1 = (mid1 > 0) ? arr1[mid1 - 1] : Integer.MIN_VALUE;
+      int l2 = (mid2 > 0) ? arr2[mid2 - 1] : Integer.MIN_VALUE;
+      int r1 = (mid1 < n1) ? arr1[mid1] : Integer.MAX_VALUE;
+      int r2 = (mid2 < n2) ? arr2[mid2] : Integer.MAX_VALUE;
+
+      if (l1 <= r2 && l2 <= r1) {
+        int l = Math.max(l1, l2);
+        int r = Math.min(r1, r2);
+        if ((n1 + n2) % 2 == 1)
+          return (double) l;
+
+        return (double) ((l + r) / 2.0);
       }
+      if (l1 > r2)
+        high = mid1 - 1;
+      else
+        low = mid1 + 1;
     }
-
-    while (i < n1) {
-      if (cnt == idx1)
-        ele1 = arr1[i];
-      if (cnt == idx2)
-        ele2 = arr1[i];
-      i++;
-      cnt++;
-    }
-    while (j < n2) {
-      if (cnt == idx1)
-        ele1 = arr2[j];
-      if (cnt == idx2)
-        ele2 = arr2[j];
-      j++;
-      cnt++;
-    }
-
-    int size = n1 + n2;
-    if (size % 2 == 0) {
-      return (ele1 + ele2) / 2.0;
-
-    }
-    return ele1;
+    return 0.0;
   }
 }
