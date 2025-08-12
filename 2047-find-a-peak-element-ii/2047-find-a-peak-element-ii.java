d5db0728b2eb27bb216445ba264476bef0d0096a
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-08-12 12:01:57 +0530

    Time: 0 ms (100%), Space: 84.4 MB (13.91%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -1,29 +1,32 @@
 class Solution {
   public int[] findPeakGrid(int[][] arr) {
-    int row = arr.length;
-    int column = arr[0].length;
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < column; j++) {
-        // checking with top element
-        if (i > 0)
-          if (arr[i][j] < arr[i - 1][j])
-            continue;
-        // checking with right element
-        if (j < column - 1)
-          if (arr[i][j] < arr[i][j + 1])
-            continue;
-        // checking with bottom element
-        if (i < row - 1)
-          if (arr[i][j] < arr[i + 1][j])
-            continue;
-        // checking with left element
-        if (j > 0)
-          if (arr[i][j] < arr[i][j - 1])
-            continue;
+    int rowsize = arr.length;
+    int columnsize = arr[0].length;
 
-        return new int[] { i, j };
+    int low = 0;
+    int high = columnsize - 1;
+
+    while (low <= high) {
+      int mid = (low + high) >> 1;
+      int max_row = 0;
+      for (int row = 0; row < rowsize; row++) {
+        if (arr[row][mid] > arr[max_row][mid])
+          max_row = row;
       }
+
+      int leftNeighbor = (mid - 1 >= 0) ? arr[max_row][mid - 1] : -1;
+      int rightNeighbor = (mid + 1 < columnsize) ? arr[max_row][mid + 1] : -1;
+
+      if (leftNeighbor < arr[max_row][mid] && rightNeighbor < arr[max_row][mid])
+        return new int[] { max_row, mid };
+
+      else if (leftNeighbor < arr[max_row][mid])
+        low = mid + 1;
+      else
+        high = mid - 1;
+
     }
+
     return new int[] { -1, -1 };
   }
 
