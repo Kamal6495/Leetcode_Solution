6a4427c220ecf923ba440346a87bd9326e982bab
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-08-20 14:14:10 +0530

    Time: 9 ms (13.95%), Space: 23.8 MB (9.78%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string token = "";
+        string res = "";
+        while (ss >> token)
+            res = token + " " + res;
+        return res.substr(0,res.length()-1);
+    }
+};
