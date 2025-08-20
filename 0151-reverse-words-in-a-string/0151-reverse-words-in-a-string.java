2c5ebfe5436e36dcfbacee478f448e9e7049a2be
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-08-20 14:26:11 +0530

    Time: 8 ms (36.42%), Space: 45.3 MB (16.98%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,37 +1,11 @@
+import java.util.*;
 class Solution {
   public String reverseWords(String s) {
-
-      int leading = 0, trailing = s.length() - 1;
-      while (leading < s.length() && s.charAt(leading) == ' ') leading++;
-      while (trailing >= 0 && s.charAt(trailing) == ' ') trailing--;
-
-      s = s.substring(leading, trailing + 1);
-
-      String res = "";
-      int a = 0;
-
-      for (int i = 0; i < s.length(); i++) {
-        if (s.charAt(i) == ' ') {
-          // avoid multiple spaces
-          if (s.charAt(i - 1) != ' ') {
-            String token = s.substring(a, i);
-            if (res.isEmpty()) {
-              res = token;
-            } else {
-              res = token + " " + res; // ✅ prepend to reverse
-            }
-          }
-          a = i + 1;
-        }
-      }
-
-      // last word (since no trailing space after last word)
-      String token = s.substring(a);
-      if (!token.isEmpty()) {
-        if (res.isEmpty()) res = token;
-        else res = token + " " + res;
-      }
-
-      return res;
+    StringTokenizer st = new StringTokenizer(s);
+    String res = "";
+    while (st.hasMoreTokens()) {
+      res = st.nextToken() + " " + res;
+    }
+    return res.substring(0, res.length() - 1);
   }
 }
