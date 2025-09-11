240ba4a673f053ec1c09bfed64120abcfc637da0
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-09-11 12:26:36 +0530

    Time: 0 ms (100%), Space: 41.6 MB (39.92%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,33 +1,18 @@
 class Solution {
-  public boolean rotateString(String s, String goal) {
-    if (s.length() != goal.length())
-      return false;
-    for (int i = 0; i < s.length(); i++) {
-      String res = leftRotate(s, i);
-      if (res.equalsIgnoreCase(goal))
-        return true;
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < s.length(); i++) {
+            // rotate by moving first char to end
+            char first = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(first);
+
+            if (sb.toString().equals(goal)) {
+                return true;
+            }
+        }
+        return false;
     }
-    return false;
-
-  }
-
-  public String leftRotate(String s, int i) {
-    char s1[] = s.toCharArray();
-    reverse(s1, 0, i);
-    reverse(s1, i + 1, s.length() - 1);
-    reverse(s1, 0, s.length() - 1);
-    return String.valueOf(s1);
-  }
-
-  public String reverse(char s[], int i, int j) {
-
-    while (i < j) {
-      char c = s[i];
-      s[i] = s[j];
-      s[j] = c;
-      i++;
-      j--;
-    }
-    return String.valueOf(s);
-  }
 }
