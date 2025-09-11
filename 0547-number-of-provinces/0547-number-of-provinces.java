0799b612615e523b289db4924494e01020ddee40
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-09-11 17:14:10 +0530

    Time: 1 ms (87.13%), Space: 48.9 MB (5.17%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,41 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int V = isConnected.length;
-        boolean[] vis = new boolean[V];
-        int cnt = 0;
+  public int findCircleNum(int[][] isConnected) {
+    int V = isConnected.length;
+    boolean[] vis = new boolean[V];
+    int cnt = 0;
 
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                cnt++;
-                dfs(isConnected, i, vis); // explore all connected cities
-            }
+    for (int i = 0; i < V; i++) {
+      if (!vis[i]) {
+        cnt++;
+        bfs(isConnected, i, vis); // explore all connected cities
+      }
+    }
+    return cnt;
+  }
+
+  private void dfs(int[][] isConnected, int src, boolean[] vis) {
+    vis[src] = true;
+
+    for (int j = 0; j < isConnected.length; j++) {
+      if (isConnected[src][j] == 1 && !vis[j]) {
+        dfs(isConnected, j, vis);
+      }
+    }
+  }
+
+  private void bfs(int[][] isConnected, int src, boolean[] vis) {
+
+    Queue<Integer> q = new LinkedList<>();
+    q.add(src);
+    while (!q.isEmpty()) {
+      int ele = q.poll();
+      for (int j = 0; j < isConnected.length; j++) {
+        if (isConnected[ele][j] == 1 && !vis[j]) {
+          q.add(j);
+          vis[j] = true;
         }
-        return cnt;
+      }
     }
 
-    private void dfs(int[][] isConnected, int src, boolean[] vis) {
-        vis[src] = true;
-
-        for (int j = 0; j < isConnected.length; j++) {
-            if (isConnected[src][j] == 1 && !vis[j]) {
-                dfs(isConnected, j, vis);
-            }
-        }
-    }
+  }
 }
