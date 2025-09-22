dba8ec7b76faa4d850bf076bdc16f69244c75b4e
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-09-22 11:49:25 +0530

    Time: 0 ms (100%), Space: 40.4 MB (66.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,16 @@
 class Solution {
-    public int climbStairs(int n) {
-        int arr[] = new int[47];
-        arr[0] = 0;
-        arr[1] = 1;
-        for (int i = 2; i < 47; i++)
-            arr[i] = arr[i - 1] + arr[i - 2];
+  public int climbStairs(int n) {
+    if (n <= 1) return 1;
 
-        return (int)arr[n + 1];
+    int prevPrev = 1;  // ways to climb 0 steps
+    int prev = 1;      // ways to climb 1 step
+    int curr = 0;
+
+    for (int i = 2; i <= n; i++) {
+      curr = prevPrev + prev;
+      prevPrev = prev;
+      prev = curr;
     }
+    return curr;
+  }
 }
