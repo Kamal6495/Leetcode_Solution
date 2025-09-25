8948d3e9bdf811dd0270b506136b50de87c79dba
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-09-25 16:46:07 +0530

    Time: 0 ms (100%), Space: 41.5 MB (11.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,34 +3,24 @@ class Solution {
     int n = nums.length;
     if (n == 1)
       return nums[0];
+    int excludeFirst = calcMoney(nums, 1, n);
 
-    int prev2 = 0;
-    int prev = nums[1];
-    // int dp[] = new int[n];
-    // dp[1] = nums[1];
-    for (int i = 2; i < n; i++) {
-      int pick = nums[i];
-      if (i > 1)
-        pick += prev2;
-      int notpick = 0 + prev;
-      int curr_i = Math.max(pick, notpick);
-      prev2 = prev;
-      prev = curr_i;
-    }
-    int excludeFirst = prev;
-
-    prev2 = 0;
-    prev = nums[0];
-    for (int i = 1; i < n - 1; i++) {
-      int pick = nums[i];
-      if (i > 1)
-        pick += prev2;
-      int notpick = 0 + prev;
-      int curr_i = Math.max(pick, notpick);
-      prev2 = prev;
-      prev = curr_i;
-    }
-    int excludeLast = prev;
+    int excludeLast = calcMoney(nums, 0, n - 1);
     return Math.max(excludeFirst, excludeLast);
+  }
+
+  int calcMoney(int nums[], int start, int end) {
+    int prev2 = 0;
+    int prev = nums[start];
+    for (int i = start+1; i < end; i++) {
+      int pick = nums[i];
+      if (i > start+1)
+        pick += prev2;
+      int notpick = 0 + prev;
+      int curr_i = Math.max(pick, notpick);
+      prev2 = prev;
+      prev = curr_i;
+    }
+    return prev;
   }
 }
