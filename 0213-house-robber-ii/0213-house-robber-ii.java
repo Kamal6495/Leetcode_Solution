676e55bc5478d6f92fac37663566737d419a06f0
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-09-25 16:40:52 +0530

    Time: 0 ms (100%), Space: 41 MB (65.27%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,29 +3,34 @@ class Solution {
     int n = nums.length;
     if (n == 1)
       return nums[0];
-    int dp[] = new int[n];
-    dp[1] = nums[1];
+
+    int prev2 = 0;
+    int prev = nums[1];
+    // int dp[] = new int[n];
+    // dp[1] = nums[1];
     for (int i = 2; i < n; i++) {
       int pick = nums[i];
       if (i > 1)
-        pick += dp[i - 2];
-      int notpick = 0 + dp[i - 1];
-      dp[i] = Math.max(pick, notpick);
-
+        pick += prev2;
+      int notpick = 0 + prev;
+      int curr_i = Math.max(pick, notpick);
+      prev2 = prev;
+      prev = curr_i;
     }
+    int excludeFirst = prev;
 
-    int excludeFirst = dp[n - 1];
-    Arrays.fill(dp, 0);
-    dp[0] = nums[0];
+    prev2 = 0;
+    prev = nums[0];
     for (int i = 1; i < n - 1; i++) {
       int pick = nums[i];
       if (i > 1)
-        pick += dp[i - 2];
-      int notpick = 0 + dp[i - 1];
-      dp[i] = Math.max(pick, notpick);
-
+        pick += prev2;
+      int notpick = 0 + prev;
+      int curr_i = Math.max(pick, notpick);
+      prev2 = prev;
+      prev = curr_i;
     }
-    int excludeLast = dp[n - 2];
+    int excludeLast = prev;
     return Math.max(excludeFirst, excludeLast);
   }
 }
