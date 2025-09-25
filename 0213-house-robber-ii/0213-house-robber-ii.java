7e54da95d849bc3a0c7e1128f7161b49ce4088c6
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-09-25 16:20:23 +0530

    Time: 0 ms (100%), Space: 40.8 MB (91.89%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,27 +3,29 @@ class Solution {
     int n = nums.length;
     if (n == 1)
       return nums[0];
-    int memo[] = new int[n];
-    Arrays.fill(memo, -1);
-    int excludeFirst = CalcMoney(nums, 0, n - 2, memo);
-    Arrays.fill(memo, -1);
-    int excludeLast = CalcMoney(nums, 1, n - 1, memo);
+    int dp[] = new int[n];
+    dp[1] = nums[1];
+    for (int i = 2; i < n; i++) {
+      int pick = nums[i];
+      if (i > 1)
+        pick += dp[i - 2];
+      int notpick = 0 + dp[i - 1];
+      dp[i] = Math.max(pick, notpick);
+
+    }
+
+    int excludeFirst = dp[n - 1];
+    Arrays.fill(dp, 0);
+    dp[0] = nums[0];
+    for (int i = 1; i < n - 1; i++) {
+      int pick = nums[i];
+      if (i > 1)
+        pick += dp[i - 2];
+      int notpick = 0 + dp[i - 1];
+      dp[i] = Math.max(pick, notpick);
+
+    }
+    int excludeLast = dp[n - 2];
     return Math.max(excludeFirst, excludeLast);
-  }
-
-  public int CalcMoney(int[] nums, int start, int idx, int memo[]) {
-
-    if (idx == start)
-      return nums[start];
-    if (idx < start)
-      return 0;
-
-    if (memo[idx] != -1)
-      return memo[idx];
-    int pick = nums[idx] + CalcMoney(nums, start, idx - 2,memo);
-    int notpick = 0 + CalcMoney(nums, start, idx - 1,memo);
-
-    memo[idx] = Math.max(pick, notpick);
-    return memo[idx];
   }
 }
