d2869555b6e98d6ede285dd32ba8ef612a1eda64
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-09-25 15:11:24 +0530

    Time: 0 ms (100%), Space: 41.3 MB (24.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,18 @@
 class Solution {
   public int rob(int[] nums) {
-    int memo[] = new int[nums.length];
-    Arrays.fill(memo,-1);
-    return CalcMoney(nums, nums.length - 1, memo);
+    int n = nums.length;
+    int dp[] = new int[n];
+    dp[0] = nums[0];
+
+    for (int i = 1; i < n; i++) {
+
+      int pick = nums[i];
+      if (i > 1)
+        pick += dp[i - 2];
+      int notpick = 0 + dp[i - 1];
+      dp[i] = Math.max(pick, notpick);
+    }
+    return dp[n - 1];
   }
 
-  public int CalcMoney(int[] nums, int idx, int memo[]) {
-    if (idx == 0)
-      return nums[idx];
-    if (idx < 0)
-      return 0;
-    if (memo[idx] != -1)
-      return memo[idx];
-    int pick = nums[idx] + CalcMoney(nums, idx - 2,memo);
-    int notpick = 0 + CalcMoney(nums, idx - 1,memo);
-    memo[idx] = Math.max(pick, notpick);
-    return memo[idx];
-  }
 }
