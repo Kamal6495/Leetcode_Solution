145b9fd7b50afc39f997a9c34f513e1e071ff768
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-10-07 11:47:17 +0530

    Time: 0 ms (100%), Space: 40.9 MB (17.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,23 @@
 class Solution {
   public int uniquePaths(int m, int n) {
-	int memo[][]=new int[m][n] ;
-  for(int i=0;i<m;i++)
-  Arrays.fill(memo[i],-1);
-		return paths(m - 1, n - 1,memo);
-	}
-
-  public int paths(int m, int n, int memo[][]) {
-    if (m == 0 && n == 0) {
-      return 1;
+    int dp[][] = new int[m][n];
+    // Initialize first row and column
+    for (int i = 0; i < m; i++)
+      dp[i][0] = 1;
+    for (int j = 0; j < n; j++)
+      dp[0][j] = 1;
+    //Iterative Reverse
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        int left = 0, right = 0;
+        if (i > 0)
+          left = dp[i - 1][j];
+        if (j > 0)
+          right = dp[i][j - 1];
+        dp[i][j] = left + right;
+      }
     }
-    // If already computed, return stored value
-    if (memo[m][n] != -1)
-      return memo[m][n];
-    int left = 0, right = 0;
-    if (m > 0)
-      left = paths(m - 1, n,memo);
-    if (n > 0)
-      right = paths(m, n - 1,memo);
-    // System.out.printf("m=%d , n=%d l=%d r=%d\n",m,n,left,right);
-    // Store result before returning
-    memo[m][n] = left + right;
-    return left + right;
+    return dp[m - 1][n - 1];
   }
+
 }
