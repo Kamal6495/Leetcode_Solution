3310028260de36e2c2d7f629bb0581d69d0683fe
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-10-08 14:44:42 +0530

    Time: 0 ms (100%), Space: 41.7 MB (82.46%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,40 +1,30 @@
 class Solution {
-  public int uniquePathsWithObstacles(int[][] arr) {
-    int m = arr.length;
-    int n = arr[0].length;
+  public int uniquePathsWithObstacles(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
     int dp[][] = new int[m][n];
-    if (arr[0][0] == 1)
+    if (grid[0][0] == 1)
       return 0;
-    dp[0][0] = 1;
+    dp[0][0] = 1;//Initail
 
-    for (int i = 1; i < m; i++) {//Row
-      if (arr[i][0] == 1)
-        dp[i][0] = 0;
-      else
-        dp[i][0] = dp[i - 1][0];
-    }
-    for (int i = 1; i < n; i++) {//Column
-      if (arr[0][i] == 1)
-        dp[0][i] = 0;
-      else
-        dp[0][i] = dp[0][i - 1];
-    }
-
-    for (int i = 1; i < m; i++) {
-
-      for (int j = 1; j < n; j++) {
-        if (arr[i][j] == 1) {
-          dp[i][j] = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 && j == 0)
           continue;
+        if (grid[i][j] == 1)
+          dp[i][j] = 0;
+        else {
+          int left = 0, right = 0;
+          if (i > 0)
+            left = dp[i - 1][j];
+          if (j > 0)
+            right = dp[i][j - 1];
+
+          dp[i][j] = left + right;
         }
-        int left = 0, right = 0;
-        if (i > 0)
-          left = dp[i - 1][j];
-        if (i > 0)
-          right = dp[i][j - 1];
-        dp[i][j] = left + right;
       }
     }
+
     return dp[m - 1][n - 1];
   }
 }
