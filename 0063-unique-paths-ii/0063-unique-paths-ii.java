93d88027d62e2113c85d1d1f17a580bdb0a556a2
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-10-08 10:45:52 +0530

    Time: 0 ms (100%), Space: 42.1 MB (19.59%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,29 +1,40 @@
 class Solution {
-  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    int m = obstacleGrid.length;
-    int n = obstacleGrid[0].length;
-    int memo[][] = new int[m][n];
-    for (int i = 0; i < m; i++)
-      Arrays.fill(memo[i], -1);
-    return f(m - 1, n - 1, obstacleGrid, memo);
-  }
-
-  public int f(int m, int n, int arr[][], int[][] memo) {
-    if (arr[m][n] == 1)
+  public int uniquePathsWithObstacles(int[][] arr) {
+    int m = arr.length;
+    int n = arr[0].length;
+    int dp[][] = new int[m][n];
+    if (arr[0][0] == 1)
       return 0;
+    dp[0][0] = 1;
 
-    if (m == 0 && n == 0)
-      return 1;
-    if (memo[m][n] != -1)
-      return memo[m][n];
-    int left = 0, right = 0;
-    if (m > 0)
-      left = f(m - 1, n, arr, memo);
-    if (n > 0)
-      right = f(m, n - 1, arr, memo);
+    for (int i = 1; i < m; i++) {//Row
+      if (arr[i][0] == 1)
+        dp[i][0] = 0;
+      else
+        dp[i][0] = dp[i - 1][0];
+    }
+    for (int i = 1; i < n; i++) {//Column
+      if (arr[0][i] == 1)
+        dp[0][i] = 0;
+      else
+        dp[0][i] = dp[0][i - 1];
+    }
 
-    memo[m][n] = left + right;
-    // return left + right;//Same working
-    return memo[m][n];
+    for (int i = 1; i < m; i++) {
+
+      for (int j = 1; j < n; j++) {
+        if (arr[i][j] == 1) {
+          dp[i][j] = 0;
+          continue;
+        }
+        int left = 0, right = 0;
+        if (i > 0)
+          left = dp[i - 1][j];
+        if (i > 0)
+          right = dp[i][j - 1];
+        dp[i][j] = left + right;
+      }
+    }
+    return dp[m - 1][n - 1];
   }
 }
