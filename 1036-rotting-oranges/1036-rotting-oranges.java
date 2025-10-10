4caaecc7739c49ee6438d7be8f673823422ab239
Kamal Kant Singh <54643090+Kamal6495@users.noreply.github.com>
2025-10-10 16:47:51 +0530

    Time: 1 ms (100%), Space: 42.3 MB (87.14%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,59 +1,56 @@
 import java.util.*;
 
 class Pair {
-  int first, second;
-
-  public Pair(int f, int s) {
-    first = f;
-    second = s;
-  }
+    int first, second, time;
+    public Pair(int f, int s, int t) {
+        first = f;
+        second = s;
+        time = t;
+    }
 }
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int total = 0; // total fresh + rotten oranges
+        int total = 0; 
         Queue<Pair> q = new LinkedList<>();
 
+        // Count total oranges and add all initially rotten oranges to queue
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2)
-                    q.add(new Pair(i, j));
+                    q.add(new Pair(i, j, 0));
                 if (grid[i][j] != 0)
                     total++;
             }
         }
 
         if (total == 0)
-            return 0; // no oranges at all
+            return 0;  // no oranges
 
-        int time = 0;
-        int rotten = q.size(); // initially rotten
+        int rotten = 0;
+        int maxTime = 0;  // to store the last timestamp
         int dr[] = {0, 0, 1, -1};
         int dc[] = {1, -1, 0, 0};
 
+        // BFS traversal
         while (!q.isEmpty()) {
-            int size = q.size();
-            boolean change = false;
-            for (int s = 0; s < size; s++) {
-                Pair P = q.poll();
-                int rr = P.first;
-                int cc = P.second;
-                for (int i = 0; i < 4; i++) {
-                    int nr = rr + dr[i];
-                    int nc = cc + dc[i];
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2;
-                        q.add(new Pair(nr, nc));
-                        rotten++;
-                        change = true;
-                    }
+            Pair p = q.poll();
+            int r = p.first, c = p.second, t = p.time;
+            rotten++;
+            maxTime = Math.max(maxTime, t);  // update time to latest rotten orange
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
+                    grid[nr][nc] = 2;
+                    q.add(new Pair(nr, nc, t + 1));
                 }
             }
-            if (change)
-                time++;
         }
 
-        return rotten == total ? time : -1;
+        return rotten == total ? maxTime : -1;
     }
 }
